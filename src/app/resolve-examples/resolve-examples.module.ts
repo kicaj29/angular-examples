2d8c6be70718c838571c802850626e23282ee8f2
Kowalski, Jacek <jacek.kowalski@dieboldnixdorf.com>
2017-08-22 10:56:14 +0200

    Resolve - created customer component

--- src/app/resolve-examples/resolve-examples.module.ts
+++ src/app/resolve-examples/resolve-examples.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResolveExamplesComponent } from './resolve-examples.component';
 import { routing } from './resolve-examples.routing';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   imports: [
     CommonModule,
     routing
   ],
-  declarations: [ResolveExamplesComponent]
+  declarations: [ResolveExamplesComponent, CustomerComponent]
 })
 export class ResolveExamplesModule { }
--- src/app/resolve-examples/resolve-examples.routing.ts
+++ src/app/resolve-examples/resolve-examples.routing.ts
@@ -1,9 +1,11 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ResolveExamplesComponent } from './resolve-examples.component';
+import { CustomerComponent } from './customer/customer.component';
 
 const routes: Routes = [
-  { path: '', component: ResolveExamplesComponent }
+  { path: '', component: ResolveExamplesComponent },
+  { path: 'customer', component: CustomerComponent }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forChild(routes);
