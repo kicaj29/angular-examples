9a18b18f5b0eae6125a6954c5483a3dfda799b10
Kowalski, Jacek <jacek.kowalski@dieboldnixdorf.com>
2017-09-02 10:07:39 +0200

    Example with reactive forms part3

--- src/app/forms-examples/reactive-froms-example/hero-detail/hero-detail.component.ts
+++ src/app/forms-examples/reactive-froms-example/hero-detail/hero-detail.component.ts
@@ -1,27 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Address, Hero, states } from './data-model';
+import { Address, Hero, states } from './../data-model';
 
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit {
+export class HeroDetailComponent implements OnChanges {
 
   heroForm: FormGroup;
-  hero: Hero;
+  @Input() hero: Hero;
   states = states;
 
   constructor(private fb: FormBuilder) {
-    this.hero = new Hero();
+/*    this.hero = new Hero();
     this.hero.name = "Super hero!!!";
-    this.hero.addresses = [];
+    this.hero.addresses = [];*/
     this.createForm();
-    this.heroForm.patchValue({
+/*    this.heroForm.patchValue({
       heroName: this.hero.name,
       address: this.hero.addresses[0] || new Address()
-    });
+    });*/
   }
 
   createForm() {
@@ -33,7 +33,12 @@ export class HeroDetailComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
+  /**Angular calls this method whenever @Input() hero changes its value.*/
+  ngOnChanges() {
+    console.log("HeroDetailComponent: ngOnChanges");
+    this.heroForm.reset({
+      heroName: this.hero.name,
+      address: this.hero.addresses[0] || new Address()
+    });
   }
-
 }
--- src/app/forms-examples/reactive-froms-example/reactive-forms-example.module.ts
+++ src/app/forms-examples/reactive-froms-example/reactive-forms-example.module.ts
@@ -4,13 +4,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { ReactiveFromsExampleComponent } from './reactive-froms-example.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule
   ],
-  declarations: [ReactiveFromsExampleComponent, HeroDetailComponent],
+  providers: [HeroService],
+  declarations: [ReactiveFromsExampleComponent, HeroDetailComponent, HeroListComponent],
   exports: [ReactiveFromsExampleComponent]
 })
 export class ReactiveFormsExampleModule { }
