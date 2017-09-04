3409ef3c5eb4a3cdaef1806a842810670d870994
Kowalski, Jacek <jacek.kowalski@dieboldnixdorf.com>
2017-09-04 13:56:11 +0200

    Forms example - not working my super control

--- src/app/forms-examples/forms-examples.module.ts
+++ src/app/forms-examples/forms-examples.module.ts
@@ -4,16 +4,19 @@ import { FormsExamplesComponent } from './forms-examples.component';
 import { routing } from './forms-examples.routing';
 import { ReactiveFormsExampleModule } from './reactive-froms-example/reactive-forms-example.module';
 import { TemplateDrivenFormsExampleModule } from './template-driven-forms-example/template-driven-forms-example.module';
-//import { MySuperControlComponent } from './my-super-control/my-super-control.component';
+import { MySuperControlComponent } from './my-super-control/my-super-control.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     routing,
+    FormsModule,
     ReactiveFormsExampleModule,
     TemplateDrivenFormsExampleModule
   ],
-  declarations: [FormsExamplesComponent/*, MySuperControlComponent*/],
+  declarations: [FormsExamplesComponent],
+  exports: [MySuperControlComponent],
   providers: []
 })
 export class FormsExamplesModule { }
--- src/app/forms-examples/reactive-froms-example/reactive-forms-example.module.ts
+++ src/app/forms-examples/reactive-froms-example/reactive-forms-example.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { ReactiveFromsExampleComponent } from './reactive-froms-example.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { HeroService } from './hero.service';
-import { MySuperControlComponent } from '../my-super-control/my-super-control.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule,
-    FormsModule //Here needed only to use ngModel in MySuperControlComponent
+    ReactiveFormsModule
   ],
   providers: [HeroService],
-  declarations: [ReactiveFromsExampleComponent, HeroDetailComponent, HeroListComponent, MySuperControlComponent],
+  declarations: [ReactiveFromsExampleComponent, HeroDetailComponent, HeroListComponent],
   exports: [ReactiveFromsExampleComponent]
 })
 export class ReactiveFormsExampleModule { }
